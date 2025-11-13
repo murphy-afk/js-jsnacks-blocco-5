86ac45138e9aacc314e13231eccd3984441d6b3a
murphy-afk <bardarenoemi@gmail.com>
2025-11-13 15:25:17 +0100

    feat: snack 1, snack 2, snack 3

--- ex-js-array-methods/snack-1/script.js
+++ ex-js-array-methods/snack-1/script.js
@@ -1,5 +1,6 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
-console.log(names);
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+const printNames = names.forEach((name) => console.log(name));
--- ex-js-array-methods/snack-2/script.js
+++ ex-js-array-methods/snack-2/script.js
@@ -6,3 +6,5 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const names = people.forEach(({ name }) => console.log(name));
