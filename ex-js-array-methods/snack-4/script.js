1e4d8a43c7000e20dab52e6d6229113d1af9a082
murphy-afk <bardarenoemi@gmail.com>
2025-11-13 15:27:17 +0100

    feat: snack 4

--- ex-js-array-methods/snack-4/script.js
+++ ex-js-array-methods/snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorNames = posts.map(({author}) => author);
+console.log(authorNames);
